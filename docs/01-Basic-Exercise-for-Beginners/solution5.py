e776927a450c3791690653c7505faa5eb26ae668
mikelis <nikolaosmikelis@gmail.com>
2023-02-17 13:30:04 +0200

    docks

---
+++ docs/01-Basic-Exercise-for-Beginners/solution5.py
@@ -0,0 +1,16 @@
+def first_last_same (numberList):
+    print("Given list:", numberList)
+    
+    first_num = numberList[0]
+    last_num = numberList[-1]
+    
+    if first_num == last_num:
+        return True
+    else:
+        return False
+
+numbers_x = [10, 20, 30, 40, 10]
+print("result is", first_last_same(numbers_x))
+
+numbers_y = [75, 65, 35, 75, 30]
+print("result is", first_last_same(numbers_y))
---
+++ docs/01-Basic-Exercise-for-Beginners/solution6.py
@@ -0,0 +1,6 @@
+numberList = [10, 20, 33, 46, 55]
+print("Given list:", numberList)
+print("Divisible by 5")
+for i in numberList:
+    if i % 5 == 0:
+        print(i)
